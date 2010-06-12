af20f9f57784d499fc307ac145006ac2cecaa382
Victor Zagorski <vzagorski@inbox.ru>
2010-06-12 17:12:36 +0800

    fixed update_post method

--- test/livejournal/livejournal.rb
+++ test/livejournal/livejournal.rb
@@ -5,30 +5,20 @@ class LiveJournalTest < Test::Unit::TestCase
     {:username=>@@username, :password=>@@password}
   end
   
-  def test_config
-    assert_equal 'victor_zagorski', @@username
-  end
+  def test_create_update_and_delete
+    assert_equal "victor_zagorski", @@username
+   
+    assert_not_nil(@lj = LiveJournal::Main.new(connection_settings))
+    
+    assert_not_nil @lj.login, "logging in"
+    
+    assert_not_nil post = @lj.create_post(:subject=>'livejournal poster test subject', :body=>'Livejournal poster test body', :security=>"private"), "creating post"
+    
+    puts "url = " + post[:url]
+  
+    assert_not_nil post = @lj.update_post(post[:itemid], :subject=>'livejournal poster updated test subject', :body=>'Livejournal poster updated test body', :security => "public"), "updating post"
 
-  def test_connection
-    assert_not_nil(LiveJournal::Main.new(connection_settings))
-  end
-  
-  def test_login
-    @lj = LiveJournal::Main.new(connection_settings)
-    assert_not_nil(@lj.login)
-  end
-  
-  def test_post
-    @lj = LiveJournal::Main.new(connection_settings)
-    assert_not_nil(post = @lj.create_post(:subject=>'livejournal poster test subject', :body=>'Livejournal poster test body'))
-    puts "url = " + post[:url]
-  end
-  
-  def test_remove
-    @lj = LiveJournal::Main.new(connection_settings)
-    post = @lj.create_post(:subject=>'livejournal poster test subject', :body=>'Livejournal poster test body. This post will be removed.')
-    puts "url = " + post[:url]
-    assert_not_nil(@lj.delete_post(post[:itemid]))
+    assert_not_nil @lj.delete_post(post[:itemid]), "deleting post"
   end
 end
 
